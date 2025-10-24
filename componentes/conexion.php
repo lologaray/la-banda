1314f2ffb73513b094a215da8119875e3f0b0966
lologaray <lorenzogaray65@gmail.com>
2025-10-24 14:16:26 -0300

    conexion

--- componentes/conexion.php
+++ componentes/conexion.php
@@ -1,14 +1,14 @@
 <?php
-$servidor = "localhots";
+$servidor = "localhost";
 $usuario = "root";
 $contrasenia = "";
 $base_datos = "agencia";
 
-//Create conexion
-$conexion = new mysqli(hostname: $servidor, username: $usuario, password: $contrasenia,database: $base_datos);
+$conexion = new mysqli($servidor, $usuario, $contrasenia, $base_datos);
 
-
-//check connection
 if ($conexion->connect_error) {
-    die("Error de conexion:  "  . $conexion->connect_error);
+    die("Error de conexión: " . $conexion->connect_error);
 }
+
+$paquetes = $conexion->query("SELECT * FROM la_banda");
+?>
