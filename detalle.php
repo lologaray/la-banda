c2261a162e2e531eb978cd7ba57fc5a3bb15c39c
lologaray <lorenzogaray65@gmail.com>
2025-11-07 14:50:48 -0300

    detalle.php

--- detalle.php
+++ detalle.php
@@ -1,11 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    HOLA! DETALLE!
-</body>
-</html>
+<?php
+require_once 'componentes/conexion.php';
+
+$id_paquete = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+if ($id_paquete > 0) {
+
+   $paquete = $conexion->query("
+      SELECT *
+      FROM paquete
+      WHERE id_paquete = $id_paquete
+      AND (estado = 'Disponible' OR estado = 'Proximamente')
+   ")->fetch_assoc();
+
+   if (!$paquete) {
+      echo "<div class='alert alert-danger text-center mt-5'>
+               Paquete no encontrado o no disponible.
+            </div>";
+      exit;
+   }
+
+} else {
+   echo "<div class='alert alert-warning text-center mt-5'>
+            ID de paquete no válido.
+         </div>";
+   exit;
+}
+?>
