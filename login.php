e0941bfe38e46d4f717fffcdabdffcf3c60ece83
Matu0906 <matuang0915@gmail.com>
2025-11-10 16:37:23 -0300

    regis56565

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
   require_once 'componente/conexion.php';
   
+  
   if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['ingresar'])) {
       $errores = '';
       $correo = $conexion->real_escape_string($_POST['nombre-usuario'] ?? '');
@@ -51,10 +52,16 @@
     <form method="POST" action="login.php">
         
     </form>
+     <?php require_once 'componentes/comp-form-ligin.php'; ?>
 
     <div>
-        <p>¿no tienes usuario? Registrate: <a</p>    
+        <p>¿no tienes usuario? Registrate: <a href="registro.php">aquí></a></p>    
     </div>
+
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
