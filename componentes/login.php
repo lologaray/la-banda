7c0e8db3003e1d7bfc05818b46b08d37d1f4d9f0
Matu0906 <matuang0915@gmail.com>
2025-11-10 15:12:22 -0300

    login2

--- componentes/login.php
+++ componentes/login.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>AGENCIA DE VIAJE - LOGIN</title>
 </head>
 <body>
+    <form method="POST" action="login.php">
+        <label for="nombre.usuario"> Nombre De Usuario:</label>
+        <input type="email" name="nombre-usuario" id="nombre-usuario" />
+
+        <label for="contraseña">contraseña</label>
+        <input type="password" name="contraseña" id="contraseña" />
+    </form>
     
 </body>
 </html>
