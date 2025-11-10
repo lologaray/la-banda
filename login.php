76209b851d321150a5c2b3d9982687b9a843df3c
Matu0906 <matuang0915@gmail.com>
2025-11-10 16:32:09 -0300

    nuevo login

--- login.php
+++ login.php
@@ -49,14 +49,7 @@
 </head>
 <body>
     <form method="POST" action="login.php">
-        <label for="nombre.usuario"> Nombre De Usuario:</label>
-        <input type="email" name="nombre-usuario" id="nombre-usuario" />
-
-        <label for="contraseña">contraseña</label>
-        <input type="password" name="contraseña" id="contraseña" />
-
-        <input type="submit" value="Ingresar" />
-
+        
     </form>
 
     <div>
