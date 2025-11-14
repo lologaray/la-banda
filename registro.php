8a3c341c607b9489507f83e898cb226c89e5c938
Matu0906 <matuang0915@gmail.com>
2025-11-14 14:20:55 -0300

    isert

--- registro.php
+++ registro.php
@@ -17,10 +17,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['ingresar'])) {
         $query->bind_param(type: 's' var:, $correo);
         $query->execute();
 
-        if($query->get_result()->num_rows){
+        if($query->get_result()->num_rows > 0) {
             $errores .= "<div class='alert alert-danger'> El correo ya está registrado.</div>";
+        }
+        if (empty($errores)) {
+            $contra_hash = password_hash(password: $contraseña, algo: PASSWORD_BCRYPT);
+            $query = $conexion->prepare('INSERT INTO usuario (email, contraseña) VALUES (?, ?)');
+            $query->bind_param(type: 'ss', var: $correo, $contraseña);
+            $sentencia = $query->execute();
+
+            $query->close();
+            $conexion->close();
+
+            if ($sentencia) {
+                $succes = "<div class='alert alert-success'> Registro exitoso. <a href='login.php'>Iniciar sesión</a></div>";
+                header("Location: index.php");
+            } else {
+                $errores .= "<div class='alert alert-danger'> Error en BBD,pruebe mas tarde.</div>";
+            }
         
         
 
       }
+    
+    }   
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
 
