94928a4b69a4e4ceefdadaf2f6bed304754cb01d
Matu0906 <matuang0915@gmail.com>
2025-11-14 14:32:46 -0300

    mmvmv

--- index.php
+++ index.php
@@ -159,6 +159,7 @@ $paquetes = $conexion->query("
     const infoDiv = document.getElementById('additional-info');
     infoDiv.style.display = (infoDiv.style.display === 'block') ? 'none' : 'block';
   }
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </script>
 
 </body>
