4d08bbb5b8b8ca7517564079773d9fccc3eea698
dylanadangacitua-cyber <dylanadangacitua@gmail.com>
2025-12-05 14:48:24 -0300

    qq

--- logOut.php
+++ logOut.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 session_start();
-
-if (session_destroy()){
-    header("Location:index.php");
-    exit;
-}
+session_unset();
+session_destroy();
+header("Location: index.php");
+exit();
 ?>
